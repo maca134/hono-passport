ce916975b0c7bd17fbe5bd7ff347d2aa043829f5
Matthew McConnell <maca134@googlemail.com>
2025-04-24 15:05:25 +0100

    feat: enhance clearUser function to support generic user and session types

--- src/clearUser.ts
+++ src/clearUser.ts
@@ -1,7 +1,7 @@
 import type { Context } from 'hono';
+import { HonoPassportEnv } from './HonoPassportEnv';
 
-
-export function clearUser(ctx: Context) {
+export function clearUser<TUser, TSessionUser>(ctx: Context<HonoPassportEnv<TUser, TSessionUser>>) {
 	const session = ctx.var.session.data;
 	if (session.__passport__) {
 		session.__passport__.user = undefined;
